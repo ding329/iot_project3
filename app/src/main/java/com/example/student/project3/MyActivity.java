2a2acd0f950b567f5d25895d529a985bffef1bcc
ding329 <ding329@yahoo.com>
2016-03-06 16:51:22 -0800

    fix spelling errors in building your first app

--- app/src/main/java/com/example/student/project3/MyActivity.java
+++ app/src/main/java/com/example/student/project3/MyActivity.java
@@ -76,7 +76,7 @@ public class MyActivity extends AppCompatActivity {
      */
     public void sendMessage(View view) {
         // Do something in response to button
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        Intent intent = new Intent(this, DisplayMessageAtivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
